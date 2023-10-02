59907306841d8b33b2c75c370b040a587a81b1a4
macbookair <play4ree@mail.ru>
2023-10-02 11:16:33 +0300

    1

--- src/main/java/app/model/AnimalsCage.java
+++ src/main/java/app/model/AnimalsCage.java
@@ -1,19 +1,31 @@
 package app.model;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class AnimalsCage {
 
-    @Autowired
-    private Animal animal;
+    private Animal animalCat;
+    private Animal animalDog;
+
+    private Timer timer;
+
+    public Timer getTimer() {
+        return timer;
+    }
+
+    public AnimalsCage(@Qualifier("cat") Animal animalCat, @Qualifier("dog") Animal animalDog, Timer timer) {
+        this.animalCat = animalCat;
+        this.animalDog = animalDog;
+        this.timer = timer;
+    }
 
     public void whatAnimalSay() {
         System.out.println("Say:");
-        System.out.println(animal.toString());
+        System.out.println(animalCat.toString());
         System.out.println("At:");
-        System.out.println(new Timer().getTime());
+        System.out.println(timer.getTime());
         System.out.println("________________________");
     }
 }
